b5bbe7e3e70f65c03b1b11174b564e51a3aad0af
CornelioCC <canocastrocornelio@gmail.com>
2025-08-05 14:55:51 -0600

    Vinculacion a postgres, dependencia agregada, creacion tablas cursos y perfiles

---
+++ src/main/resources/db/migration/V1__create-table-cursos.sql
@@ -0,0 +1,7 @@
+create table cursos(
+    id SERIAL PRIMARY KEY,
+    nombre varchar(100) not null,
+    categoria varchar(100) not null
+
+
+);
---
+++ src/main/resources/db/migration/V2__create-table-perfiles.sql
@@ -0,0 +1,5 @@
+create table perfiles(
+    id SERIAL PRIMARY KEY,
+    nombre varchar(100) not null
+
+);
